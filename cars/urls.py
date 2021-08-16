8ebcae17aa9f121fde781feca0fd42fadb333bbd
odai1990 <odai22odai78@gmail.com>
2021-08-16 08:58:47 +0300

    fixing

--- cars/urls.py
+++ cars/urls.py
@@ -25,8 +25,7 @@ from django.urls import path,include
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),   
     path('api/v1/cars/',include('carsStore.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
